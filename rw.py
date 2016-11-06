04fb40435f6868a4f39a84f75c5b2fd24ccdcefb
jun wei <ngjunwei.15@gmail.com>
2016-11-07 00:03:48 +0800

    Update rw.py
    - Corrected read to not append None frames
    - Added videowriter for write function

--- rw.py
+++ rw.py
@@ -11,39 +11,39 @@ import numpy as np
 
 
 def read(vid_name):
-	""" Read video frames
-	
-	Args:
-		vid_name (string): Name of video file to be read
+    """
+    Purpose:
+        Read video frames
+    Args:
+        vid_name (string): Name of video file to be read
+    Returns:
+        frames (ndarray): Ndarray of all frames read from video
+    """
+    vid = cv2.VideoCapture(vid_name)
+    frames_list = []
 
-	Returns:
-		frames (ndarray): Ndarray of all frames read from video
+    ret = True
+    while ret:
+        ret, frame = vid.read()  # Read frame by frame
+        if frame is not None:
+            frames_list.append(frame)  # Appends frames read from video
+            #
+            # # Terminates when all frames are read
+            # if ret is False:
+            #     break
 
-	"""
-	vid = cv2.VideoCapture(vid_name)
-	frames_list = []
+    fps = vid.get(cv2.cv.CV_CAP_PROP_FPS)
 
-	while True:
-	    ret, frame = vid.read()		# Read frame by frame
-	    frames_list.append(frame)	# Appends frames read from video
+    # Release capture once all frames are read and appended
+    vid.release()
 
-	    # Terminates when all frames are read
-	    if ret == False:
-	    	break
+    # Converts array to ndarray
+    frames = np.asarray(frames_list)
 
-	# Release capture once all frames are read and appended
-	vid.release()
-
-	# Converts array to ndarray
-	frames = np.asarray(frames_list)
-
-	return frames
-
-
-def write():
-	"""
-	TODO: Write function
-	"""
-	return None
+    return frames, fps
 
 
+def write(filename, frames, fps):
+    # type: (string, ndarray, int) -> void
+    (h, w) = frames[0].shape[:2]
+    vid = cv2.VideoWriter(filename + ".avi", cv2.cv.CV_FOURCC('D', 'I', 'V', 'X'),  fps, (w, h))
